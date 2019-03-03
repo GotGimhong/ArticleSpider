c460a5478a1e1e8696a9bcadb70e46f596523b90
GotGimhong <260612483@qq.com>
2019-03-04 00:38:05 +0800

    Correct upperbound of range() and add duplication check.

--- src/lib/urlreader.py
+++ src/lib/urlreader.py
@@ -28,8 +28,7 @@ def read(fileName):
     tindex  = -1
     pindex = -1
     uindex  = -1
-    count = sheet.ncols - 1
-    for index in range(0, count):
+    for index in range(0, sheet.ncols):
         head = sheet.col(index)[0].value
         if re.match(__pattern_title, head, re.I):
             tindex = index
@@ -42,10 +41,27 @@ def read(fileName):
     assert(tindex >= 0 and pindex >= 0 and uindex >= 0)
     # Set up a table with publish time and title as key and url as value.
     table = {}
-    count = sheet.nrows - 1
-    for index in range(1, count):
+    result = {}
+    for index in range(1, sheet.nrows):
+        title = __correct(sheet.cell(index, tindex).value)
         pdate = sheet.cell(index, pindex).value
         pdate = pdate[0 : pdate.find(" ")].replace("-", "")
-        title = __correct(sheet.cell(index, tindex).value)
-        table["%s@%s" % (pdate, title)] = sheet.cell(index, uindex).value
-    return table
+        # Check duplication.
+        if title in table:
+            table[title] += 1
+        else:
+            table[title] = 1
+            result["%s@%s" % (pdate, title)] = sheet.cell(index, uindex).value
+    # Record duplication.
+    dup = 0
+    with open("201812.txt", "a", encoding = "utf-8") as file:
+        file.write("标题 : 重复次数\n")
+        for key, value in table.items():
+            if value == 1:
+                continue
+            file.write("%s : %d\n" % (key, value - 1))
+            dup += (value - 1)
+        file.write("实际导出数 : %d = 数据总数 : %d - 重复总数 : %d\n" % (len(table), sheet.nrows - 1, dup))
+        file.close()
+    del table
+    return result
